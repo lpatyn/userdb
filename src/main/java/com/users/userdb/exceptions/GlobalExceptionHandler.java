d427be346217a03e7a5bd64b2875b2eb8ef54fcb
lpatyn <lautaropatyn@outlook.com>
2025-01-02 18:01:43 -0300

    Cleaned some unused imports

--- src/main/java/com/users/userdb/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/users/userdb/exceptions/GlobalExceptionHandler.java
@@ -5,15 +5,12 @@ import com.users.userdb.dto.response.KeyValueErrorDTO;
 import com.users.userdb.dto.response.ValidationErrorDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
